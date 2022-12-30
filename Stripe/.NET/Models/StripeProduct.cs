c49e7652e6244cc0c5dd05885a345e7d02ae681b
JacobHelton1997 <111300828+JacobHelton1997@users.noreply.github.com>
2022-12-30 15:22:49 -0500

    Create StripeProduct.cs

---
+++ Stripe/.NET/Models/StripeProduct.cs
@@ -0,0 +1,9 @@
+    public class StripeProduct
+    {
+        public string Name { get; set; }
+        public string ProductId { get; set; }
+        public string PriceId { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+  
+    }

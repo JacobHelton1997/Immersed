0687a167b8132a3ac64c03c7883ca58c4976f2be
JacobHelton1997 <111300828+JacobHelton1997@users.noreply.github.com>
2022-12-30 15:25:50 -0500

    Create PaymentAccountAddRequest.cs

---
+++ Stripe/.NET/Requests/PaymentAccountAddRequest.cs
@@ -0,0 +1,6 @@
+    public class PaymentAccountAddRequest
+    {
+        public int VendorId { get; set; }
+        public string AccountId { get; set; }
+        public int PaymentTypeId { get; set; }
+    }

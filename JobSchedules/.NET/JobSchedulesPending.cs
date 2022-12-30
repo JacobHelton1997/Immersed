cab77e31f237680660edb072a7fcdacbb9a6de7d
JacobHelton1997 <111300828+JacobHelton1997@users.noreply.github.com>
2022-12-30 14:28:54 -0500

    Create JobSchedulesPending.cs

---
+++ JobSchedules/.NET/JobSchedulesPending.cs
@@ -0,0 +1,7 @@
+public class JobSchedulesPending
+    {
+        public List<JobSchedule> Daily { get; set; }
+        public List<JobSchedule> Weekly { get; set; }
+        public List<JobSchedule> Monthly { get; set; }
+        public List<JobSchedule> Yearly { get; set; }
+    }

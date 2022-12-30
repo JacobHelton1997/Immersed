4925c4af677424e73eb6fcf4d84a0213b9b44740
JacobHelton1997 <111300828+JacobHelton1997@users.noreply.github.com>
2022-12-30 14:35:14 -0500

    Create JobControllerSettings.cs

---
+++ JobSchedules/.NET/Quartz/JobControllerSettings.cs
@@ -0,0 +1,14 @@
+public class JobControllerSettings: IJobControllerSettings
+    {
+        public int GetCurrentDay()
+        {
+            int currentDay = (int)DateTime.Now.DayOfWeek;
+            return currentDay;
+        }
+
+        public int GetCurrentHour()
+        {
+            int currentHour = (int) DateTime.UtcNow.Hour;
+            return currentHour;
+        }
+    }
